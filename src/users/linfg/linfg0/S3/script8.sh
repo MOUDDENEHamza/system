e1123701fd5c9ee4bba6c38c5c0cde87fc4f124b
hamza <hz.mouddene@gmail.com>
2019-02-19 21:01:53 +0100

    TP3 DONE

--- src/users/linfg/linfg0/S3/script8.sh
+++ src/users/linfg/linfg0/S3/script8.sh
@@ -1,38 +1,43 @@
 #!/bin/sh
 
 #Write an interactif script able to navigate on the Linux system.
-dir=`ls -ld */ | wc -l`
-echo "Current repository : "`pwd`"\n" 
 
-for i in `seq 1 $dir`
-do
-	echo $i")- "`ls -d */` >> listrepo
-done
-cat listrepo
+navigate() {
+	cpt=0
+	touch listrepo
+	dir=`ls -ld */ | wc -l`
+	echo "Current repository : "`pwd`"\n" 
+	ls -d */ | while read line ; do
+		cpt=$((cpt + 1))
+		echo $cpt")- "$line >> listrepo
+	done
+	cat listrepo
+	echo
+}
 
+echo "Start of script ...\n"
+navigate
 while [ 1 = 1 ] ; do
 	read -p "Input the number of the repository where you want to go : " number
+	echo
 	if [ $number = -2 ] ; then
+		echo "\nEnd of script ..."
 		rm listrepo
 		exit 0
 	fi
 	if [ $number = -1 ] ; then
-		rm listrepo
 		cd ../
-		echo "Current repository : "`pwd`"\n"
-		dir=`ls -ld */ | wc -l`
-	        for i in `seq 1 $dir` ;do
-        	        echo $i")- "`ls -d */` >> listrepo
-		done
-		cat listrepo
+		rm listrepo
+		navigate
 		continue
 	fi
+	if [ $number = 0 ] ; then
+		echo "Current repository : "`pwd`"\n"
+		cat listrepo
+		echo
+		continue
+        fi
 	cd `grep $number listrepo | cut -f 2 -d " "`
-	dir=`ls -ld */ | wc -l`
-	for i in `seq 1 $dir` ;do
-        	echo $i")- "`ls -d */` >> listrepo
-	done
-	pwd
-	rm listrepo
+	rm ../listrepo
+	navigate
 done
-rm listrepo
